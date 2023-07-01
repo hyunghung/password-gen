e8715ea9d2f34f9c9617e617776f49e81501197b
hyunghung <hyunghungnguyen@example.com>
2023-06-30 20:54:03 -0700

    Completed changes to: generating password (adding special characters and taking in user's input

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,64 @@
 // Assignment code here
-var howLong = prompt("How many characters would you like your password to contain?");
 var minNum = 8;
 var maxNum = 128;
-
-console.log(howLong);
-console.log(minNum);
-console.log(maxNum);
-
-function compare(){
-  if(howLong <= minNum || maxNum <= howLong){
-    confirm("Choose a length larger than 8 characters!");
-    var howLong = prompt("How many characters would you like your password to contain?");
-  }
-  else return howLong;
-};
-
-
-compare();
-// var specialChar = confirm("Click OK to confirm including special characters.")
-// var numbers = confirm("Click OK to confirm including numbers.");
-// var lowerCase = confirm("Click OK to confirm including lowercase characters.");
-// var upperCase = confirm("Click OK to confirm including uppercase characters.");
-
-
+var possibleChar = "";
+let genPassword = "";
+let length;
 //generate password
-function generatePassword(){
+function generatePassword(length, specialChar,numbers,lowerCase,upperCase){
+  //Listing all the possible character that can be used within the password
+  const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const specialChars = "!@#$%^&*()_-+=<>?/{}[]|";
+  const numbersChars = "0123456789";
 
+
+//Setting conditions that were made before, if user pressed "cancel" then nothing will be made 
+// Otherwise if they clicked "OK" then those characters would be added into the password
+  if (specialChar){
+    possibleChar += specialChars;
+  }
+  if(numbers){
+    possibleChar += numbersChars;
+  }
+  if(lowerCase){
+    possibleChar += lowerCaseChars;
+  }
+  if(upperCase){
+    possibleChar += upperCaseChars;
+  }
+  if(!possibleChar){
+    alert("Please choose at least one thing");
+  }
+
+// Incrementing the code so that it takes in random charcters and smashing them together
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * possibleChar.length);
+    genPassword += possibleChar[randomIndex];
 }
+// return to make new of the variable 
+return genPassword;
+}
+
+
+
+
+while(true){
+  let howLong = prompt("How many characters would you like your password to contain?");
+  length = parseInt(howLong);
+  if(length < minNum || length > maxNum ){
+    confirm("Choose a length between 8 and 128 characters!!");
+  }
+     else break;
+}
+  const specialChar = confirm("Click OK to confirm including special characters.")
+  const numbers = confirm("Click OK to confirm including numbers.");
+  const lowerCase = confirm("Click OK to confirm including lowercase characters.");
+  const upperCase = confirm("Click OK to confirm including uppercase characters.");
+//}
+generatePassword(length, specialChar,numbers,lowerCase,upperCase);
+// writePassword();
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
